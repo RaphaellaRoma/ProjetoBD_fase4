6517db199cc7dc56cc59f5fc41d6463284a29e2b
Raphaella Roma Mendes Alves <162490655+RaphaellaRoma@users.noreply.github.com>
2025-06-08 10:26:14 -0300

    Verificação das informações em db_config.py

--- db_config.py
+++ db_config.py
@@ -3,11 +3,11 @@ import psycopg2
 def conectar():
     try:
         conn = psycopg2.connect(
-            host="10.61.49.170",       # IP do servidor (verifique se está certo)
-            port="5432",               # Porta padrão do PostgreSQL
-            database="amazon",         # Nome do seu banco
-            user="misterio_amazon",           # Usuário
-            password="amazon123"  # Coloque sua senha real
+            host="10.61.49.170",
+            port="5432",
+            database="amazon",
+            user="misterio_amazon",
+            password="amazon123"
         )
         print("Conexão bem-sucedida!")
         return conn
