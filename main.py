9cddb1530d4fd0354bc46a2014ac6afd922cf2fa
Bia Marques <santosmarquesbeatriz@gmail.com>
2025-06-09 18:12:55 -0300

    tabelas loja, cli_loja e cli_prod prontas

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from produto_crud import janela_produto
 from transportadora_crud import janela_transportadora
 from loja_crud import janela_loja
 from cli_loja import janela_cli_loja
+from cli_prod import janela_cli_prod
 from consultas import janela_consultas
 
 def abrir_cliente():
@@ -25,6 +26,9 @@ def abrir_loja():
 
 def abrir_cli_loja():
     janela_cli_loja()
+
+def abrir_cli_prod():
+    janela_cli_prod()
 
 def abrir_consultas():
     janela_consultas()
@@ -47,11 +51,14 @@ btn_pedido.pack(pady=10)
 btn_pedido = tk.Button(root, text="Gerenciar Transportadora", command=abrir_transportadora, width=30)
 btn_pedido.pack(pady=10)
 
-btn_pedido = tk.Button(root, text="Gerenciar Loja", command=abrir_loja, width=30)
-btn_pedido.pack(pady=10)
+btn_loja = tk.Button(root, text="Gerenciar Loja", command=abrir_loja, width=30)
+btn_loja.pack(pady=10)
 
-btn_pedido = tk.Button(root, text="Gerenciar Cliente Avalia Loja", command=abrir_cli_loja, width=30)
-btn_pedido.pack(pady=10)
+btn_cli_loja = tk.Button(root, text="Gerenciar Cliente Avalia Loja", command=abrir_cli_loja, width=30)
+btn_cli_loja.pack(pady=10)
+
+btn_cli_prod = tk.Button(root, text="Gerenciar Cliente Avalia Produto", command=abrir_cli_prod, width=30)
+btn_cli_prod.pack(pady=10)
 
 btn_consultas = tk.Button(root, text="Consultas SQL", command=abrir_consultas, width=30)
 btn_consultas.pack(pady=10)
