595471f3bac569b739dae6c26c929a2faeede7dc
Raphaella Roma <raphysics.oficial@gmail.com>
2025-06-10 08:01:24 -0300

    Corrigi um dos testes

--- index/indice_pedido_cliente.sql
+++ index/indice_pedido_cliente.sql
@@ -2,10 +2,34 @@
 
 -- TESTE 1
 
--- Sem índice
+-- Sem Índice
+
+-- Desabilita uso de index scan para forçar uso de varredura sequencial
 SET enable_indexscan = off;
 EXPLAIN ANALYZE
-SELECT * FROM amazon.cliente WHERE NomeCliente ILIKE 'A%';
+SELECT * FROM amazon.pedido WHERE ClienteID = 200;
+-- Habilita de novo após o teste
+SET enable_indexscan = on;
+
+-- Índice para otimizar a consulta de pedidos por cliente
+CREATE INDEX idx_pedido_cliente ON amazon.pedido (ClienteID);
+
+-- Comando de verificação de performance depois
+EXPLAIN ANALYZE SELECT * FROM amazon.pedido WHERE ClienteID = 200;
+
+
+
+-- TESTE 2
+
+-- Sem índice
+SET enable_indexscan = off;
+
+EXPLAIN ANALYZE
+SELECT *
+FROM amazon.pedido
+WHERE ClienteID = 15
+ORDER BY DataPedFeito;
+
 SET enable_indexscan = on;
 
 -- Índice para otimizar a consulta de pedidos por cliente
@@ -13,16 +37,9 @@ CREATE INDEX idx_pedido_cliente ON amazon.pedido (ClienteID);
 
 -- Comando de verificação de performance depois
 EXPLAIN ANALYZE
-SELECT * FROM amazon.cliente WHERE NomeCliente ILIKE 'A%';
+SELECT *
+FROM amazon.pedido
+WHERE ClienteID = 15
+ORDER BY DataPedFeito;
 
 
--- TESTE 2
-
--- Sem Índice
-SET enable_indexscan = off;
-EXPLAIN ANALYZE
-SELECT * FROM amazon.pedido WHERE ClienteID = 200;
-SET enable_indexscan = on;
-
--- Comando de verificação de performance depois
-EXPLAIN ANALYZE SELECT * FROM amazon.pedido WHERE ClienteID = 200;
