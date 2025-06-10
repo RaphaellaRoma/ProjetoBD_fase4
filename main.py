dfbe5af9a374f9f6708c8595138e98ad797a5ed3
g4briwlle <gabriellemascarelo@gmail.com>
2025-06-09 21:40:25 -0300

    atualizando main usando os novos cruds feitos, lembrando que quantia de produto ta comentada

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from produto_crud import janela_produto
 from transportadora_crud import janela_transportadora
 from loja_crud import janela_loja
 from cli_loja import janela_cli_loja
+from ped_prod_crud import janela_itens_pedido
+# from ped_quant_crud import janela_ped_quant
 from consultas import janela_consultas
 
 def abrir_cliente():
@@ -29,6 +31,12 @@ def abrir_cli_loja():
 def abrir_consultas():
     janela_consultas()
 
+def abrir_ped_prod(): 
+    janela_itens_pedido()
+
+# def abrir_ped_quant():
+#     janela_ped_quant()
+
 root = tk.Tk()
 root.title("Sistema de Gerenciamento - Amazon")
 root.geometry("400x500")
@@ -44,6 +52,12 @@ btn_cliente.pack(pady=10)
 btn_pedido = tk.Button(root, text="Adicionar Pedido", command=abrir_pedido, width=30)
 btn_pedido.pack(pady=10)
 
+btn_ped_prod = tk.Button(root, text="Gerenciar Itens do Pedido", command=abrir_ped_prod, width=30)
+btn_ped_prod.pack(pady=10) 
+
+# btn_ped_quant = tk.Button(root, text="Gerenciar Quantidade do Pedido", command=abrir_ped_quant, width=30)
+# btn_ped_quant.pack(pady=10)
+
 btn_pedido = tk.Button(root, text="Gerenciar Transportadora", command=abrir_transportadora, width=30)
 btn_pedido.pack(pady=10)
 
@@ -55,5 +69,6 @@ btn_pedido.pack(pady=10)
 
 btn_consultas = tk.Button(root, text="Consultas SQL", command=abrir_consultas, width=30)
 btn_consultas.pack(pady=10)
+
 
 root.mainloop()
